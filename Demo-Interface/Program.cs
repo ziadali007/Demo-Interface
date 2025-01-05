181aade59581c656a3ac8be8d57c3f8c4268367e
ziad <ziadalimohammed812@gmail.com>
2025-01-05 07:33:26 +0200

    Add project files.

---
+++ Demo-Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
